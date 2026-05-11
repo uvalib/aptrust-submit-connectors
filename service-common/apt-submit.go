9fec1ca7953eafac466dc9edd7aaafbb0f4be279
Dave G <dpg3k@virginia.edu>
2026-05-11 09:26:32 -0400

    Grrr...

--- service-common/apt-submit.go
+++ service-common/apt-submit.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/config"
+        "github.com/seqsense/s3sync/v2"
 )
 
 func submitBagContents(cfg *ServiceConfig, httpClient *http.Client, localDir string, bagName string) error {
